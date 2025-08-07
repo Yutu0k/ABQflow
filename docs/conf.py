0345912a4b20659301c2beb5c0ebe5d08d3f9833
yutu0k <yutu0k@sjtu.edu.cn>
2025-08-07 23:59:42 +0800

    [doc] improve conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,12 +9,23 @@
 import os
 import sys
 import furo
-sys.path.insert(0, os.path.abspath('../..'))
+import toml
+from pathlib import Path
 
-project = 'Abaqus Batch Package'
-copyright = '2025, yutu0k'
-author = 'yutu0k'
-release = '0.2.0'
+sys.path.insert(0, os.path.abspath('../..'))
+print(os.getcwd())
+
+# --- 从 pyproject.toml 读取项目元数据 ---
+pyproject_path = Path(__file__).parents[1] / "pyproject.toml"
+with pyproject_path.open("r", encoding="utf-8") as f:
+	pyproject_data = toml.load(f)
+
+project_metadata = pyproject_data["project"]
+
+project = project_metadata["name"]
+author = ", ".join(author["name"] for author in project_metadata["authors"])
+copyright = f'2025, {author}'
+release = project_metadata["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
